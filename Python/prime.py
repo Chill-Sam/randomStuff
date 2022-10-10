3f4e4fb917c9e1097208789408d4256a8d49d37b
Chill-Sam <105503193+Chill-Sam@users.noreply.github.com>
2022-10-10 08:56:42 +0200

    Update prime.py

--- Python/prime.py
+++ Python/prime.py
@@ -1,1 +1,1 @@
-print( list( x for x in range( 2, int( input( "Pick a number: " ) ) ) if all( map( lambda s: x % s, range( 2, x ) ) ) ) )
+print( list( x for x in range( 2, int( input( "Pick a number: " ) ) + 1 ) if all( map( lambda s: x % s, range( 2, x ) ) ) ) )
